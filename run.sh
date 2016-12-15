93dcd561c4e5ee7d1fddffcfac48adc07fdd126c
Alex Palmatier <asp78@drexel.edu>
2016-12-15 14:10:38 -0500

    Changes run script to check if something is already running on the db port

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash  
 
-cd ~/dynamoDB
-java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+isListening=`netstat -an | grep 8000 | grep LISTEN | awk -F " " '{print $NF}'`
+
+if [ ! $isListening == 'LISTEN' ]
+then
+  cd ~/dynamoDB
+  java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+fi
