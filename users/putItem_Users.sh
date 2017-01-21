e955b39ca94d22974c78f2d1ceabed0c7d1eb185
Aaron Segal <aaronmsegal@gmail.com>
2017-01-21 13:48:13 -0500

    added role to user stuff

--- users/putItem_Users.sh
+++ users/putItem_Users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws dynamodb put-item --table-name users --item file://item.json --return-consumed-capacity TOTAL --endpoint-url http://localhost:8000
+aws dynamodb put-item --table-name users --item file://item3.json --return-consumed-capacity TOTAL --endpoint-url http://localhost:8000
